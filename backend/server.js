0f3a08ef63360e56120580e1f2cc945bf060ef68
vkakda <vjcse12@gmail.com>
2025-10-23 13:51:13 +0530

    add updates

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.SERVER_URL,
     methods: ["GET", "POST"],
   },
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Chat from "./components/Chat";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
